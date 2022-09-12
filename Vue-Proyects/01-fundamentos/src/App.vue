a79b1e80cecc183bac3eeab700027519a325b710
Alejandro Rivera <riveraalejandro1999@gmail.com>
2022-09-12 11:01:32 -0600

    Indecision App start

--- Vue-Proyects/01-fundamentos/src/App.vue
+++ Vue-Proyects/01-fundamentos/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div> 
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Counter :start="10" />
+    <!-- <Counter :start="100"/> -->
+    <Indecision/>
   </div>
 </template>
 
 <script>
-import Counter from "@/components/Counter.vue";
+import Indecision from "@/components/Indecision.vue";
+import Counter from "./components/Counter.vue";
 
 export default {
   name: "App",
-  components: { Counter },
+  components: { Indecision, /* Counter */},
 };
 </script>
 
