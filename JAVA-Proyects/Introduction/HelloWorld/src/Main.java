5a456ac79803eda9e8cbf81628fca18a4b36cea9
Alejandro Rivera <riveraalejandro1999@gmail.com>
2023-03-01 13:52:43 -0600

    Creacion de Hola Mundo

--- JAVA-Proyects/Introduction/HelloWorld/src/Main.java
+++ JAVA-Proyects/Introduction/HelloWorld/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
+        String saludar = "Hola Mundo desde Java";
+
         System.out.println("Alejandro Enrique Rivera Vasquez");
+        System.out.println("saludar = " + saludar.toUpperCase());
+        
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        boolean valor = true;
+        int numero2 = 5;
+        if (valor) {
+            System.out.println("numero2 = " + numero2);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+        var numero3 = "15";
     }
 }
