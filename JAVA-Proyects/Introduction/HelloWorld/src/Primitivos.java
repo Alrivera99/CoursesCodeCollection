e65172de85bba523c5daeeb795ce5252fa06e08f
Alrivera99 <riveraalejandro1999@gmail.com>
2023-03-05 22:10:01 -0600

    PrimitivosFloat

--- JAVA-Proyects/Introduction/HelloWorld/src/Primitivos.java
+++ JAVA-Proyects/Introduction/HelloWorld/src/Primitivos.java
@@ -22,6 +22,29 @@ public class Primitivos {
         System.out.println("Valor minimo tipo int " + Integer.MIN_VALUE);
         System.out.println("Valor maximo tipo int " + Integer.MAX_VALUE);
 
+        long numeroLong = 9223372036854775807L;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("Tipo long corresponde en byte a " + Long.BYTES);
+        System.out.println("Tipo long corresponde en bites a " + Long.SIZE);
+        System.out.println("Valor minimo tipo long " + Long.MIN_VALUE);
+        System.out.println("Valor maximo tipo long " + Long.MAX_VALUE);
+
+        float numeroFloat = 3.4028235E38F;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("Tipo float corresponde en byte a " + Float.BYTES);
+        System.out.println("Tipo float corresponde en bites a " + Float.SIZE);
+        System.out.println("Valor minimo tipo float " + Float.MIN_VALUE);
+        System.out.println("Valor maximo tipo float " + Float.MAX_VALUE);
+
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("Tipo double corresponde en byte a " + Double.BYTES);
+        System.out.println("Tipo double corresponde en bites a " + Double.SIZE);
+        System.out.println("Valor minimo tipo double " + Double.MIN_VALUE);
+        System.out.println("Valor maximo tipo double " + Double.MAX_VALUE);
+
+
+
 
 
 
---
+++ JAVA-Proyects/Introduction/HelloWorld/src/PrimitivosFloat.java
@@ -0,0 +1,8 @@
+public class PrimitivosFloat {
+
+    public static void main(String[] args) {
+
+        float realFloat = 3.4028235E38F;
+        System.out.println("realFloat = " + realFloat);
+    }
+}
