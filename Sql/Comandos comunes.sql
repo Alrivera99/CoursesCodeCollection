3694f64cb79eeec79c23e56c2db0f59c15e3604c
Alejandro Enrique Rivera Vasquez <aerivera@innovacion.gob.sv>
2024-06-21 14:31:46 -0600

    Comando comunes iniciales

--- Sql/Comandos comunes.sql
+++ Sql/Comandos comunes.sql
@@ -1,14 +1,36 @@
+-- Create a table
 CREATE TABLE "users"
 (
-    id   SERIAL,
-    name VARCHAR(100) NOT NULL,
-    role VARCHAR(15)  NOT NULL,
+    id    SERIAL,
+    name  VARCHAR(100)        NOT NULL,
+    role  VARCHAR(15)         NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO "users" (name, role)
-VALUES ('Alice', 'admin'),
-       ('Bob', 'user'),
-       ('Charlie', 'user');
+-- Insert some data
+INSERT INTO "users" (name, role, email)
+VALUES ('Alice', 'admin', ' [email protected]2');
 
+-- Query the table
+SELECT *
+FROM "users"
+ORDER BY id
+OFFSET 0;
+
+-- Update a row
+UPDATE "users"
+SET role = 'admin'
+WHERE id = 1;
+
+WITH nums AS (
+    SELECT id, 'prueba' || row_number() over (ORDER BY id) || '@gmail.com' as new_email
+    FROM "users"
+)
+UPDATE "users"
+SET email = nums.new_email
+FROM nums
+WHERE "users".id = nums.id;
+
+-- Delete a row
 DROP TABLE IF EXISTS "users";
